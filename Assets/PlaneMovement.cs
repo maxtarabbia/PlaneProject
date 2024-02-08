95afacf5dfa4ecf77baa20fa6568183a4eba54d2
maxtarabbia <maxtarabbia@icloud.com>
2024-02-08 14:22:40 -0600

    Fixed collision wonkyness

--- Assets/PlaneMovement.cs
+++ Assets/PlaneMovement.cs
@@ -18,17 +18,24 @@ public class PlaneMovement : MonoBehaviour
     public float yawCorrectionPower = 3f;
     public float PitchCorrectionPower = 10f;
 
+    ParticleSystem PS;
+    public bool Throttle = false;
+
+    float BonkResetTimer = 1; 
     // Start is called before the first frame update
     void Start()
     {
+        PS = GetComponentInChildren<ParticleSystem>();
         RB = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        BonkResetTimer += Time.deltaTime;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
-
 
         textMeshProUGUI.text = ("Speed: " + RB.velocity.magnitude.ToString("0.00"));
 
@@ -56,11 +63,20 @@ public class PlaneMovement : MonoBehaviour
     }
     public void DoPlaneMovement()
     {
-        if (Input.GetAxisRaw("Throttle") > 0.1)
+        if (Input.GetAxisRaw("Throttle") > 0.1 && BonkResetTimer > 1)
         {
             RB.AddRelativeForce(Vector3.forward * Thrust);
+            Throttle = true;
+        }
+        else
+        {
+            Throttle = false;
         }
 
+        if (Throttle)
+            PS.Play();
+        else if(!Throttle && PS.isPlaying)
+            PS.Stop();
 
         //Ground Check
         if (gameObject.transform.position.y <= -50 && RB.velocity.y < 0)
@@ -72,19 +88,20 @@ public class PlaneMovement : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        print("Hit Obj");
+        BonkResetTimer = 0;
+        print("Hit Obj at " + RB.velocity.magnitude);
     }
     public void DoAOACheck()
     {
-        Vector3 AOAalongAxis = Quaternion.Inverse(gameObject.transform.rotation) * RB.velocity.normalized;
+        Vector3 AOAalongAxis = Quaternion.Inverse(gameObject.transform.rotation) * RB.velocity.normalized * RB.velocity.magnitude/50f;
 
-        RB.AddRelativeTorque(new Vector3(-AOAalongAxis.y, AOAalongAxis.x, 0) * 1f); 
+        RB.AddRelativeTorque(new Vector3(-AOAalongAxis.y, AOAalongAxis.x, 0) * Mathf.Clamp01(BonkResetTimer)); 
 
         //gameObject.transform.eulerAngles += new Vector3(-AOAalongAxis.y, AOAalongAxis.x, 0) * velocity.magnitude * Time.deltaTime * 100f;
 
-        textMeshProUGUI.text += "\nPressureStrength on Plane Rotation: " + (new Vector3(-AOAalongAxis.y, AOAalongAxis.x, 0).magnitude * RB.velocity.magnitude *100f).ToString("0.0");
+        textMeshProUGUI.text += "\nPressureStrength on Plane Rotation: " + (new Vector3(-AOAalongAxis.y, AOAalongAxis.x, 0).magnitude *100f).ToString("0.0");
 
-        float pressureStrength = (Mathf.Abs(AOAalongAxis.x) * yawCorrectionPower + Mathf.Abs(AOAalongAxis.y) * PitchCorrectionPower) *Time.deltaTime;
+        float pressureStrength = (Mathf.Abs(AOAalongAxis.x) * yawCorrectionPower + Mathf.Abs(AOAalongAxis.y) * PitchCorrectionPower) * Mathf.Clamp01(BonkResetTimer);
 
         RB.velocity = Vector3.LerpUnclamped(RB.velocity, (gameObject.transform.rotation * Vector3.forward) * RB.velocity.magnitude,
             pressureStrength);
